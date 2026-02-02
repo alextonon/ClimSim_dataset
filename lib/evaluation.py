070272b92808d8bc6326bc9cffcd9cf7021a638f
Alexandre Tonon <a.tonon65@gmail.com>
2026-02-02 01:52:18 +0100

    final commit

--- lib/evaluation.py
+++ lib/evaluation.py
@@ -65,20 +65,26 @@ class MetricsEvaluator:
             self.stats[sname]["sum_y_sq"] += np.sum(yt**2, axis=(0, 1))
             self.stats[sname]["count"] += yt.shape[0] * yt.shape[1]
 
-    def finalize(self):
-        results = {}
-        for var in self.target_vars:
-            sname = self._short_name(var)
-            s = self.stats[sname]
-            n = s["count"]
-            if n == 0: continue
+    def finalize(self, return_profiles=True):
+            results = {}
+            for var in self.target_vars:
+                sname = self._short_name(var)
+                s = self.stats[sname]
+                n = s["count"]
+                if n == 0: continue
 
-            mae_levels = s["sum_abs_err"] / n
-            ss_tot = s["sum_y_sq"] - (s["sum_y"]**2 / n)
-            r2_levels = 1 - (s["ss_res"] / (ss_tot + self.eps))
+                mae_levels = s["sum_abs_err"] / n
+                
+                ss_tot = s["sum_y_sq"] - (s["sum_y"]**2 / n)
+                r2_levels = 1 - (s["ss_res"] / (ss_tot + self.eps))
 
-            results[var] = {
-                "mae": np.mean(mae_levels),
-                "r2": np.mean(r2_levels)
-            }
-        return results
+                results[var] = {
+                    "mae": np.mean(mae_levels),   
+                    "r2": np.mean(r2_levels)         
+                }
+                
+                if return_profiles:
+                    results[var]["mae_levels"] = mae_levels
+                    results[var]["r2_levels"] = r2_levels
+                    
+            return results
